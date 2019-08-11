0e823a206d26927531984f023599ce13c3d0f223
marvinzhan <marvin.zhan@limebike.com>
2019-08-11 15:45:29 -0700

    remove pod and add gitignore

--- Pods/Target Support Files/Pods-DoorDash/Pods-DoorDash-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DoorDash : NSObject
-@end
-@implementation PodsDummy_Pods_DoorDash
-@end
--- Pods/Target Support Files/SwiftLocation/SwiftLocation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftLocation : NSObject
-@end
-@implementation PodsDummy_SwiftLocation
-@end
